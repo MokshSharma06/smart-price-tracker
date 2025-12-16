37ebc9125da411a3ddf25ef502baa8194a1f55c6
moksh <mokshsharma06@gmail.com>
2025-12-16 22:06:48 +0530

    updated path of data loader to cloud

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,38 +1,56 @@
 from pyspark.sql import SparkSession
-from src.utils import get_spark_session
+from src.utils import get_spark_session, load_config
+import yaml
 from src.logger import Log4j
-from pyspark.sql.functions import col, coalesce, regexp_replace, to_timestamp ,lit
-from pyspark.sql.types import DoubleType , StructType, StructField,StringType,IntegerType
+from pyspark.sql.functions import col, coalesce, regexp_replace, to_timestamp, lit
+from pyspark.sql.types import (
+    DoubleType,
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+)
 
-spark = get_spark_session()
+config = load_config()
+container_name = config["azure"]["container_name"]
+storage_account_name = config["azure"]["account_name"]
 
-df = spark.read\
-    .option("mode", "DropMalformed")\
-    .option("multiline","True")\
-    .json("data/raw/flipkart_products.json")\
-    
-# df.printSchema()
-# df.show(truncate=True)
-rc=df.count()
-print(f"count is = {rc}")
+BASE_ADLS_PATH = (
+    f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/Data/"
+)
 
-df2 = spark.read\
-     .option("mode","DropMalformed")\
-    .option("multiline","true")\
-    .json("data/raw/ajio_products.json")\
 
-df2 = df2.drop("error") \
-         .withColumnRenamed("mrp", "mrp_price") \
-         .withColumnRenamed("price", "selling_price")
+def load_combined_data(spark, logger, base_adls_path):
+    FLIPKART_RAW_PATH = f"{base_adls_path}raw/flipkart/"
+    AJIO_RAW_PATH = f"{base_adls_path}raw/ajio/"
 
-# df2.printSchema()
-# df2.show()
-# rc2=df2.count()
-# print(f"count is = {rc2}")
+    # 1. Load Flipkart Data
+    df = (
+        spark.read.option("mode", "DropMalformed")
+        .option("multiline", "True")
+        .json(FLIPKART_RAW_PATH)
+    )
+    logger.info("Reading from Flipkart raw data")
+    rc = df.count()
+    logger.info(f"Flipkart record count = {rc}")
 
-# combining the data frames in order to gather data at one place for the downstream consumption
-combined_df = df.unionByName(df2)
-count = combined_df.count()
-# combined_df.show(n = combined_df.count(), truncate= 20)
-# combined_df.printSchema()
-# print(f"count is = file1 + file 2= {count}")
+    # 2. Load Ajio Data
+    df2 = (
+        spark.read.option("mode", "DropMalformed")
+        .option("multiline", "true")
+        .json(AJIO_RAW_PATH)
+    )
+    logger.info("Reading from Ajio raw data")
+    df2 = (
+        df2.drop("error")
+        .withColumnRenamed("mrp", "mrp_price")
+        .withColumnRenamed("price", "selling_price")
+    )
+
+    # 3. Combine DataFrames
+    logger.info("Combining dataframes using unionByName...")
+    combined_df = df.unionByName(df2, allowMissingColumns=True)
+    count = combined_df.count()
+    logger.info(f"Combined record count (Flipkart + Ajio) = {count}")
+
+    return combined_df
